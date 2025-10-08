8eb2f91ba913779ae6046a1ad0910b503c672399
Mi <me.haibubble@gmail.com>
2025-10-08 18:45:16 +0700

    Update allowed_origins.js

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = (process.env.APP_ENV == 'PROD')
-    ? [`http://localhost:${process.env.APP_PORT}`, 'https://dev.higenncy.com']
+    ? [`http://localhost:${process.env.APP_PORT}`, 'https://laundry-ku.vercel.app']
     : [`http://localhost:${process.env.APP_PORT}`, `http://localhost:4000`, `https://localhost:4000`]
 
 module.exports = { allowedOrigins }
